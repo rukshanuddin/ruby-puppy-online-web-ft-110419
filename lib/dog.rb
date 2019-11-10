908aacf46f515ad3a64113ad3e1a08a58c10b011
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-10 18:12:46 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
-# Add your code here
+class Dog
+
+  attr_accessor :name
+    @@all = []
+    @@names = []
+  def initialize(name)
+    @name = name
+    @@all << self
+    @@names << self.name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+
+  def self.print_all
+    print @@names.uniq.join("\n")
+
+  end
+
+end
